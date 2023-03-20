0ad6916818a62e6ab77d9dc031f2312db15fa30b
José Benedito dos Santos Neto <jbneto.ep@gmail.com>
2023-03-20 19:24:56 -0300

    Finalizando codigo e criando outra forma de resolver o desafio

---
+++ PontosNaCarteira/src/pontosNaCarteira2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; 
+
+public class pontosNaCarteira2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int velocidade = leitor.nextInt();
+        System.out.println("Qual a velocidade do carro?");
+        int multas = leitor.nextInt();
+        System.out.println("Quantas multas ele tem?");
+        // TODO: Crie a condição necessária para verificar se os carros estão acima da
+        // velocidade
+        if (velocidade > 80) {
+            multas++;
+        }
+        if (multas >= 3) {
+            System.out.println(multas + " multas. Levou pontos na carteira");
+        } else {
+            System.out.println(multas + " multas. Nao levou pontos na carteira");
+        }
+    }
+}
